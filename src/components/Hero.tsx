6923f87281bd6ac629d21d45d3e5d33ceb00d674
fabry-js <vivaldifabrizio10@gmail.com>
2023-08-17 10:16:48 +0200

    feat: :sparkles: add more adaptivity and base presentation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,38 +1,139 @@
 "use client";
 
+import React, { useRef } from "react";
 import { Box, Button, Flex, Grid, Heading, Img, Text } from "@chakra-ui/react";
-import React from "react";
-import { BsArrowDown } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { useParallax } from "react-scroll-parallax";
 
 function Hero() {
+  const parallaxScroll = useParallax<HTMLImageElement>({
+    rotate: [360, 0],
+  });
+
+  const aboutme = useRef<HTMLHeadingElement>(null);
+
   return (
-    <Box p="2rem">
+    <Box p={{ xxsm: "2rem", xsm: "1.5rem" }}>
       <Grid
         gridTemplateColumns={"repeat(12, 1fr)"}
         alignItems={"center"}
-        gap={{ base: "2rem", md: "unset" }}
+        gap={{ xxsm: "0" }}
       >
         <Flex
-          gridColumn={{ base: "3 / span 8", md: "2 / span 4" }}
-          textAlign={{ base: "center", md: "unset" }}
+          gridColumn={{ xxsm: "3 / span 8", xl: "2 / span 5" }}
+          textAlign={{ xxsm: "center", xl: "unset" }}
           direction={"column"}
           gap={"4rem"}
         >
           <Flex direction={"column"} gap="2.25rem">
-            <Heading fontSize={{ base: "4rem", md: "7rem" }}>
+            <Heading
+              bgGradient="linear(to-r, #00B5D8, #AE29B3)"
+              bgClip="text"
+              fontSize={{ xxsm: "3rem", xsm: "3.9rem", md: "5rem", xl: "8rem" }}
+            >
               Fabrizio Piperno
             </Heading>
-            <Text fontSize={"2rem"}>web developer based in Milazzo, Italy.</Text>
+            <motion.div
+              whileTap={{
+                scale: 0.9,
+              }}
+            >
+              <Button
+                variant={"outline"}
+                colorScheme="blue"
+                transition={"all 1s ease"}
+                color={"blue.400"}
+                size={"xl"}
+                w={{ xxsm: "85%", md: "70%", xl: "80%" }}
+                fontSize={{ xxsm: "1rem", md: "1.25rem", xl: "1.625rem" }}
+                h={{ xxsm: "3rem", md: "3.5rem", xl: "4.5rem" }}
+                onClick={() =>
+                  aboutme.current?.scrollIntoView({ behavior: "smooth" })
+                }
+              >
+                <Text
+                  bgGradient="linear(to-r, #00B5D8, #AE29B3)"
+                  bgClip={"text"}
+                >
+                  discover more
+                </Text>
+              </Button>
+            </motion.div>
           </Flex>
-          
         </Flex>
         <Img
-          gridColumn={{ base: "3 / span 8", md: "8 / span 5" }}
-          src="/geometric/two_2.png"
+          gridColumn={{
+            xxsm: "1 / span 12",
+            md: "3 / span 8",
+            lg: "4 / span 6",
+            xl: "8 / span 5",
+          }}
+          mt={{ xxsm: "1rem" }}
+          ref={parallaxScroll.ref}
+          src="/geometric/three_3.png"
         />
       </Grid>
-      <Box mt={{ md: "2rem" }}>
-        <Text>web developer based in Milazzo, Italy.</Text>
+      <Box
+        display={"grid"}
+        gridTemplateColumns={"repeat(12, 1fr)"}
+        mt={{ xxsm: "1rem", xl: "4rem" }}
+      >
+        <Heading
+          fontSize={{ xxsm: "3rem", md: "4rem", xl: "5.5rem" }}
+          bgGradient="linear(to-r, #00B5D8, #AE29B3)"
+          bgClip="text"
+          gridColumn={{ xxsm: "3 / span 8" }}
+          textAlign={"center"}
+          ref={aboutme}
+        >
+          about me
+        </Heading>
+        <Text
+          color="#AE29B3"
+          fontSize={{ xxsm: "1rem", md: "1.5rem", xl: "1.625rem" }}
+          textAlign={"center"}
+          gridColumn={{ xxsm: "3 / span 8", lg: "4 / span 6" }}
+          mt={{ xxsm: "1rem", xl: "2rem" }}
+        >
+          I am an experienced frontend developer specializing in web
+          development. <br />
+          <br /> My expertise lies in utilizing technologies such as TypeScript,
+          React, and Vue to craft high-performance, contemporary web
+          applications. <br />
+          <br /> Beyond my professional pursuits, I am fond of music and video
+          games. During my leisure time, I channel my passion into playing the
+          piano and drums.
+        </Text>
+      </Box>
+      <Box
+        display={"grid"}
+        gridTemplateColumns={"repeat(12, 1fr)"}
+        mt={{ xxsm: "1rem", xl: "4rem" }}
+      >
+        <Heading
+          fontSize={{ xxsm: "3rem", md: "4rem", xl: "5.5rem" }}
+          bgGradient="linear(to-r, #00B5D8, #AE29B3)"
+          bgClip="text"
+          gridColumn={{ xxsm: "3 / span 8" }}
+          textAlign={"center"}
+        >
+          vision
+        </Heading>
+        <Text
+          color="#AE29B3"
+          fontSize={{ xxsm: "1rem", md: "1.5rem", xl: "1.625rem" }}
+          textAlign={"center"}
+          gridColumn={{ xxsm: "3 / span 8", lg: "4 / span 6" }}
+          mt={{ xxsm: "1rem", xl: "2rem" }}
+        >
+          Harmony is the fundamental concept that drives my overall process of
+          delivering the digital products that I make, giving me the opportunity
+          to balance my unleashing creativity with the directives that I get.
+          <br />
+          <br />
+          Accessibility is also a crucial element which holds the power to
+          extend the usage of my products to potentially everyone
+        </Text>
       </Box>
     </Box>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,15 +12,23 @@ function Navbar() {
       justifyContent={"space-between"}
       p={"2rem"}
     >
-      <Img src="/firmozza.png" w="120px" h="120px" />
+      <Img
+        src="/firmozzaBlack.png"
+        filter={
+          "invert(48%) sepia(97%) saturate(1659%) hue-rotate(153deg) brightness(98%) contrast(101%)"
+        }
+        w={{ base: "60px", md: "120px" }}
+        h={{ base: "60px", md: "120px" }}
+      />
       <Button
         variant={"ghost"}
         aria-label="Menu"
         rightIcon={<AiOutlineMenu />}
-        size="lg"
+        size={{ base: "md", md: "lg" }}
         fontWeight={"light"}
         fontSize={"1.5rem"}
         rounded={"2xl"}
+        color="#AE29B3"
       >
         Menu
       </Button>
